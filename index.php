3e68c34721f15b8c5a99e21929b5d1bb62b6d8a5
poporule <82127179+poporule@users.noreply.github.com>
2023-09-28 20:57:29 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
-#
-#<?php 
+
+<?php 
 #include("includes/includedFiles.php"); 
+
+echo '<h1> Salope du ghetto  </h1>'
+
 #test 1 2 3
 #?>
 
-#<script> openPage("browse.php")</script>
-echo penis
+<!-- script> openPage("browse.php")</script -->
+
