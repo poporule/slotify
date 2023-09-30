0dba5bc1a677454b975013e50e64df725b9bfc7d
Vincent Dugas <vincentdugas@hotmail.com>
2023-09-30 12:48:38 -0300

    Updated connect

--- includes/config.php
+++ includes/config.php
@@ -5,7 +5,7 @@
 	
 	$timezone = date_default_timezone_set("Europe/London");
 
-	$con = mysqli_connect("localhost", "vince", "12", "slotify");
+	$con = mysqli_connect("192.168.2.175", "vince", "C!v!c4553631231", "slotify");
 
 	if(mysqli_connect_errno()) {
 		echo "Failed to connect: " . mysqli_connect_errno();
